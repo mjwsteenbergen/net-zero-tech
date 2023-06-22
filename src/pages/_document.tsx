c58b15af182c84570b09abb1195fa208eed797c1
Martijn Steenbergen <m.uses+git@outlook.com>
2023-06-22 19:38:03 +0200

    Add tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Net Zero Tech</title>
+        <meta name="og:title" content="Net Zero Tech" />
+        <meta name="description" content="Net zero tech is a podcast where a PhD student from the University of Oxford interviews researchers to find out: Are EVs actually helping? What is geological carbon storage? And more.." />
+        <meta name="og:description" content="Net zero tech is a podcast where a PhD student from the University of Oxford interviews researchers to find out: Are EVs actually helping? What is geological carbon storage? And more.." />
+        <meta name="description" content="Net zero tech is a podcast where a PhD student from the University of Oxford interviews researchers to find out: Are EVs actually helping? What is geological carbon storage? And more.." />
+        <meta name="twitter:description" content="Net zero tech is a podcast where a PhD student from the University of Oxford interviews researchers to find out: Are EVs actually helping? What is geological carbon storage? And more.." />
+        <link rel="alternate" type="application/rss+xml" title="Net Zero Tech Feed" href="https://feed.podbean.com/snigdhalal/feed.xml" />
+      </Head>
       <body>
         <Main />
         <NextScript />
