0e235bb86518b5b2ba1a8857a695ffc4d415bd73
Martijn Steenbergen <m.uses+git@outlook.com>
2023-06-22 19:42:53 +0200

    Add all the tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,12 +4,25 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* <!-- HTML Meta Tags --> */}
         <title>Net Zero Tech</title>
-        <meta name="og:title" content="Net Zero Tech" />
         <meta name="description" content="Net zero tech is a podcast where a PhD student from the University of Oxford interviews researchers to find out: Are EVs actually helping? What is geological carbon storage? And more.." />
-        <meta name="og:description" content="Net zero tech is a podcast where a PhD student from the University of Oxford interviews researchers to find out: Are EVs actually helping? What is geological carbon storage? And more.." />
-        <meta name="description" content="Net zero tech is a podcast where a PhD student from the University of Oxford interviews researchers to find out: Are EVs actually helping? What is geological carbon storage? And more.." />
+
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://net-zero-tech.co.uk" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Net Zero Tech" />
+        <meta property="og:description" content="Net zero tech is a podcast where a PhD student from the University of Oxford interviews researchers to find out: Are EVs actually helping? What is geological carbon storage? And more.." />
+        <meta property="og:image" content="https://i.scdn.co/image/ab67656300005f1f827985387096ee815bf6e8a3" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="net-zero-tech.co.uk" />
+        <meta property="twitter:url" content="https://net-zero-tech.co.uk" />
+        <meta name="twitter:title" content="Net Zero Tech" />
         <meta name="twitter:description" content="Net zero tech is a podcast where a PhD student from the University of Oxford interviews researchers to find out: Are EVs actually helping? What is geological carbon storage? And more.." />
+        <meta name="twitter:image" content="https://i.scdn.co/image/ab67656300005f1f827985387096ee815bf6e8a3" />
+
         <link rel="alternate" type="application/rss+xml" title="Net Zero Tech Feed" href="https://feed.podbean.com/snigdhalal/feed.xml" />
       </Head>
       <body>
